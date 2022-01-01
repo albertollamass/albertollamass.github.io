a05b3834d8180e863fdd99ac16c203e4dfcd6897
Alberto Llamas <59082768+albertollamass@users.noreply.github.com>
2022-01-02 00:31:15 +0100

    Added ppt

--- js/scripts.js
+++ js/scripts.js
@@ -16,13 +16,13 @@
 
 
     /* Countdown Timer - The Final Countdown */
-	$('#clock').countdown('2022/1/9 18:00:00') /* change here your "countdown to" date */
+	$('#clock').countdown('2022/1/9 22:00:00') /* change here your "countdown to" date */
 	.on('update.countdown', function(event) {
 		var format = '<span class="counter-number">%D<br><span class="timer-text">Days</span></span><span class="counter-number">%H<br><span class="timer-text">Hours</span></span><span class="counter-number">%M<br><span class="timer-text">Minutes</span></span><span class="counter-number">%S<br><span class="timer-text">Seconds</span></span>';
 		$(this).html(event.strftime(format));
 	})
 	.on('finish.countdown', function(event) {
-	$(this).html('enlace a ppt')
+	$(this).html('https://docs.google.com/presentation/d/1eroW9j7mxUT-jhIMNX1U2m2BkyR6fRWb/edit?usp=sharing&ouid=108671370337121084456&rtpof=true&sd=true')
 		.parent().addClass('disabled');
     });
 
